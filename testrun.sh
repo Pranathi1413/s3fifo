55707b6d79d881d8d461d680d106e0ffd1cf0759
Pranathi1413 <pranathiwuppuluru@gmail.com>
2025-12-10 03:04:38 -0500

    Run

--- testrun.sh
+++ testrun.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-trace_dir="libCacheSim/data1"
-out_dir="results1"
-mkdir -p "$out_dir"
+MANIFEST="trace.manifest"
+CACHESIM="./libCacheSim/_build/bin/cachesim"
+ALGO="s3fifo"
+SIZES="0.001,0.01,0.1"
+THREADS="${SLURM_CPUS_PER_TASK:-1}"
+OUTDIR="results1"
+mkdir -p "$OUTDIR"
 
-for trace in "$trace_dir"/*; do
-	base="$(basename "$trace")"
-	out="$out_dir/${base%%.*}.txt"
-	echo "\nRunning $trace and storing result in $out"
-	./libCacheSim/_build/bin/cachesim "$trace" oracleGeneral s3fifo 0 --ignore-obj-size 1 > "$out" 2>&1
-	echo "\nDone running $trace"
+N=$(wc -l < "$MANIFEST")
+echo "$N traces in total"
+proc=${SLURM_PROCID:-0}
+ntasks=${SLURM_NTASKS:-1}
+echo "This is proc $SLURM_PROCID"
+
+for ((i=proc; i<N; i+=ntasks)); do
+  trace=$(sed -n "$((i+1))p" "$MANIFEST")
+  filename="${trace##*/}"
+  outname="${filename%%.*}"
+  echo "Starting $trace Output in $outname"
+  "$CACHESIM" "$trace" oracleGeneral "$ALGO" "$SIZES" --ignore-obj-size 1 --num-thread "$THREADS" > "$OUTDIR/${outname}-${ALGO}.txt" 2>&1
+  echo "Done $trace"
 done
